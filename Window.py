f70026f02679e1c4f7fcaba80d91701c2f8d83fd
marbibu <marbibu@gmail.com>
2014-06-03 20:57:06 +0200

    Pierwsza wersja klasy, która tworzy okno programu
    Służy do utworzenia okna programu

---
+++ Window.py
@@ -0,0 +1,23 @@
+from Tkinter import Tk
+class Window:
+      #Klasa, ktora rysuje okno
+      def __init__(s,title,w,h,x,y):
+            #Dane:
+            s.__title=title
+            s.__w,s.__h=w,h
+            s.__x,s.__y=x,y
+            #Definicje:
+            s.__draw()
+      def __draw(s):#Rysuje okno
+            s.__master=Tk()
+            s.__master.title(s.__title)
+            s.__master.geometry("%sx%s+%s+%s"%(s.__w,s.__h,s.__x,s.__y))
+      def getTitle(s):#Zwraca tytul
+            return s.__title
+      def setTitle(s,title):#Ustawia tytul
+            s.__title=title
+            s.__master.title(title)
+      def loop(s):#Zapetla wyswietlanie okna
+            s.__master.mainloop()
+      def getMaster(s):#Zwraca id okna
+            return s.__master
